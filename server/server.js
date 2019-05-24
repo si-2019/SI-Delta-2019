fba124d0a6118b8de74b0dedf42e89703e486870
Asja Horozic <ahorozic1@etf.unsa.ba>
2019-05-24 16:57:49 +0200

    port

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const port = process.env.PORT || 31903;
+const port = process.env.PORT || 31904;
 
 
 
