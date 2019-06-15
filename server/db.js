f8dc641e99db48f14d82d252ce75db810e0da643
Amer Kadić <akadic5@etf.unsa.ba>
2019-06-15 19:38:48 +0200

    update

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const sequelize = new Sequelize("TYQcLL35gV","TYQcLL35gV","BLysSj9ZrP",{ host:"3
 //const sequelize = new Sequelize("hXb0pLcW9N","hXb0pLcW9N","3VeAJRjpCp",{ host:"37.59.55.185", dialect:"mysql"});
 
 const db={};
-//3VeAJRjpCp
+
 db.Sequelize = Sequelize;  
 db.sequelize = sequelize;
 
@@ -16,6 +16,9 @@ db.IspitiRezultati = sequelize.import("../models/ispiti_rezultati.js");
 db.IspitBodovi = sequelize.import("../models/IspitBodovi.js");
 db.PredmetStudent=sequelize.import("../models/predmet_student.js");
 db.Zadaca=sequelize.import("../models/Zadaca.js");
+db.PrisustvoTutorijali=sequelize.import("../models/PrisustvoTutorijali.js");
+db.PrisustvoPredavanja=sequelize.import("../models/PrisustvoPredavanja.js");
+
 
 db.Student.belongsToMany(db.Ispit, {through: 'IspitiRezultati'});
 db.Ispit.belongsToMany(db.Student, {through: 'IspitiRezultati'});
